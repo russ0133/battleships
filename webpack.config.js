df29988b36bb4dbb26209c1c45006da9f6a7798d
Gabriel <gabrieldsfs@gmail.com>
2022-02-24 01:18:38 +0500

    (test)

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,19 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/i,
+        include: path.resolve(__dirname, "src"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         test: /\.css$/i,
         include: path.resolve(__dirname, "src"),
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
